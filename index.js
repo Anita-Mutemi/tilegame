e35c46652412532e410454fa06b951c5a0816d0b
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-05-23 12:37:59 +0300

    working on the responsiveness of stop button

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ const movesCounter = () => {
             <h4>Moves: ${movesCount}</h4>`;
               stopGame();
             }
+            //innerHTML
           } else {
           
             let [tempFirst, tempSecond] = [activeTile, secondTile];
